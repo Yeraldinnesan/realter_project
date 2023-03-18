1f22dce501a72c18713f8be116d1dc217305d500
Yeraldinnesan <ysanabria92@hotmail.com>
2023-03-18 11:28:44 -0500

    build: :construction: db conection and main html templates

--- realter/urls.py
+++ realter/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
