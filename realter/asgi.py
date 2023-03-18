09c6c923812e45d3e60784f44981828d9dffcd3b
Yeraldinnesan <ysanabria92@hotmail.com>
2023-03-17 22:50:28 -0500

    first commit

---
+++ realter/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for realter project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realter.settings')
+
+application = get_asgi_application()
