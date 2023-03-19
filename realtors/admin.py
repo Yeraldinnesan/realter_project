b3965dfc95f30f21577118633fbe904022c19195
Yeraldinnesan <ysanabria92@hotmail.com>
2023-03-18 20:53:06 -0500

    feat: :sparkles: listings main

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Realtor
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'email', 'hire_date' )
+    list_display_links = ('id', 'name')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    list_per_page = 25
+
+admin.site.register(Realtor, RealtorAdmin)
