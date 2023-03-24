52f41336f3e1819e7c2498650216031abdfe01fd
Yeraldinnesan <ysanabria92@hotmail.com>
2023-03-24 11:28:49 -0500

    feat: :sparkles: User Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
+from contacts.models import Contact
+
 def register(request):
     if request.method =="POST":
         # Get form values
@@ -59,4 +61,9 @@ def logout(request):
         return redirect('index')
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+    context = {
+        'contacts': user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context)
